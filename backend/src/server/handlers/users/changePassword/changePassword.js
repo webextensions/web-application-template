6eebc530528566cbf94b89896a561e9cfaef313d
priyank <priyank@example.com>
2025-01-09 20:53:59 +0400

    General maintenance

--- backend/src/server/handlers/users/changePassword/changePassword.js
+++ backend/src/server/handlers/users/changePassword/changePassword.js
@@ -24,29 +24,35 @@ const changePassword = function ({ constructorParamForUsers }) {
                     console.error('Error: User not found', userUuid);
                     return sendErrorResponse(res, 404, 'User not found');
                 }
+
                 case 'OLD_PASSWORD_INCORRECT': {
                     console.error('Error: Old password incorrect', userUuid);
                     return sendErrorResponse(res, 401, 'Invalid old password');
                 }
+
                 // // TODO: Uncomment this case when/if the 'NEW_PASSWORD_SAME_AS_OLD' related functionality is implemented
                 // case 'NEW_PASSWORD_SAME_AS_OLD': {
                 //     console.error('Error: New password is same as old password', userUuid);
                 //     return sendErrorResponse(res, 400, 'New password is same as old password');
                 // }
-                // // TODO: Uncomment this case when/if the 'PASSWORD_WEAK' related functionality is implemented
-                // case 'PASSWORD_WEAK': {
-                //     console.error('Error: New password is weak', userUuid);
-                //     return sendErrorResponse(res, 400, 'New password is weak');
-                // }
+
                 case 'NEW_PASSWORD_DOES_NOT_MEET_POLICY': {
                     console.error('Error: New password does not meet policy', userUuid);
                     return sendErrorResponse(res, 400, 'New password does not meet policy');
                 }
+                // // TODO: Uncomment this case when/if the 'PASSWORD_WEAK' related functionality is implemented
+                // //       Note: 'PASSWORD_WEAK' may be a sub-case of 'NEW_PASSWORD_DOES_NOT_MEET_POLICY'
+                // case 'PASSWORD_WEAK': {
+                //     console.error('Error: New password is weak', userUuid);
+                //     return sendErrorResponse(res, 400, 'New password is weak');
+                // }
+
                 // // TODO: Uncomment this case when/if the 'PASSWORD_RESET_REQUIRED' related functionality is implemented
                 // case 'PASSWORD_RESET_REQUIRED': {
                 //     console.error('Error: Password reset required', userUuid);
                 //     return sendErrorResponse(res, 400, 'Password reset required');
                 // }
+
                 default: {
                     console.error('Error: Unable to change password', userUuid, err);
                     return sendErrorResponse(res, 500, 'Internal Server Error');
